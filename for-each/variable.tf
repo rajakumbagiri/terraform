e8a2caac88bb5e18dedb403baaf6247724f20560
rajakumbagiri <kumbagiri.raja75.com>
2025-08-29 23:32:55 +0530

    each

---
+++ for-each/variable.tf
@@ -0,0 +1,24 @@
+variable "instance_names" {
+  type        = map
+  default     = {
+    db = "t3.small"
+    backend = "t3.micro"
+    frontend = "t3.micro"
+  }
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "Expense"
+        Terraform = "true"
+    }
+}
+
+variable "domain_name" {
+    default = "rajakumbagiri.online"
+}
+
+variable "zone_id" {
+    default = "Z04534625Q6QIBHSF856"
+}
